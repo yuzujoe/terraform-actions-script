c5492cc6af810769a1b92ef26fdde41b653c12ba
yuzujoe <39491874+yuzujoe@users.noreply.github.com>
2021-01-23 18:18:48 +0900

    Setup terraform actions (#1)
    * setup terraform sample define
    
    * add actions script
    
    * fix format
    
    * add modules
    
    * add plan scripts
    
    * add job step
    
    * test commit
    
    * debug
    
    * fix script
    
    * fix script
    
    * fix script

---
+++ aws/sample-1/versions.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.24.0"
+    }
+  }
+  required_version = "0.14.5"
+}
